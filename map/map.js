ed685fb6f69c449a3b23ba5494f29274b59c400b
Sean Luce <lucesean@gmail.com>
2023-03-07 11:06:39 -0500

    corrected CMK regions

--- map/map.js
+++ map/map.js
@@ -263,7 +263,7 @@ var regionlist = [
         "avsdatastore": true,
         "backup": false,
         "largevolumes": true,
-        "cmk": true
+        "cmk": false
     },
     {
         "shortname": "northeurope",
